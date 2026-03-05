a7e4d9156dbc85483e9537a73cb81da3572afe4f
Leo Fades <137497080+LeoFades@users.noreply.github.com>
2026-03-05 18:12:50 +0800

    Particle background

--- protfolio_steve/src/components/ui/Background/Background.tsx
+++ protfolio_steve/src/components/ui/Background/Background.tsx
@@ -1,8 +1,23 @@
 import styles from './Background.module.css'
+import Particles from '../Particles/Particles'
+
 function Background() {
     return (
 
         <div className={styles.background}>
+            <div className={styles.particles}>
+                <Particles
+                    particleColors={["#ffffff"]}
+                    particleCount={200}
+                    particleSpread={10}
+                    speed={0.1}
+                    particleBaseSize={100}
+                    moveParticlesOnHover
+                    alphaParticles={false}
+                    disableRotation={false}
+                    pixelRatio={1}
+                />
+            </div>
         </div>
 
 
