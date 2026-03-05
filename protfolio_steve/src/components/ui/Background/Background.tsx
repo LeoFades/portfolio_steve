4a4cd897e5c5479902152f049bea6f9a73c19124
Leo Fades <137497080+LeoFades@users.noreply.github.com>
2026-03-06 01:25:37 +0800

    grainient alternative bg

--- protfolio_steve/src/components/ui/Background/Background.tsx
+++ protfolio_steve/src/components/ui/Background/Background.tsx
@@ -1,11 +1,12 @@
 import styles from './Background.module.css'
-import Particles from '../Particles/Particles'
+// import Particles from '../Particles/Particles'
+import Grainient from '../Grainient/Grainient'
 
 function Background() {
     return (
 
         <div className={styles.background}>
-            <div className={styles.particles}>
+            {/* <div className={styles.particles}>
                 <Particles
                     particleColors={["#ffffff"]}
                     particleCount={200}
@@ -16,6 +17,33 @@ function Background() {
                     alphaParticles={false}
                     disableRotation={false}
                     pixelRatio={1}
+                />
+            </div> */}
+
+            <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+                <Grainient
+                    color1="#0a0609"
+                    color2="#320f3d"
+                    color3="#0f0826"
+                    timeSpeed={0.25}
+                    colorBalance={0}
+                    warpStrength={1}
+                    warpFrequency={5}
+                    warpSpeed={2}
+                    warpAmplitude={50}
+                    blendAngle={0}
+                    blendSoftness={0.05}
+                    rotationAmount={500}
+                    noiseScale={2}
+                    grainAmount={0.1}
+                    grainScale={2}
+                    grainAnimated={false}
+                    contrast={1.5}
+                    gamma={1}
+                    saturation={1}
+                    centerX={0}
+                    centerY={0}
+                    zoom={0.9}
                 />
             </div>
         </div>
